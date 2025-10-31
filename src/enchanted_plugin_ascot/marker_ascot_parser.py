e433630de5c148553ee9f5f4d42c8a4e2f523917
Daniel Jordan <danieljordan@mahti-login11.mahti.csc.fi>
2025-10-31 14:55:34 +0200

    some backbone code for Otso to use to make marker runner for his work

---
+++ src/enchanted_plugin_ascot/marker_ascot_parser.py
@@ -0,0 +1,8 @@
+
+
+class MarkerAscotParser():
+    def __init__(self):
+        None
+    
+    def read_marker_config(self, marker_config_path):
+        None
---
+++ src/enchanted_plugin_ascot/marker_ascot_runner.py
@@ -0,0 +1,16 @@
+from enchanted_surrogates.runners.base_runner import Runner
+
+class MarkerAscotRunner(Runner):
+    def __init__(self, marker_config_path, plamsa_path, bfield_path, options_path, *args, **kwargs):
+    self.marker_config_path = marker_config_path
+    self.plamsa_path = plamsa_path
+    self.bfield_path = bfield_path
+    self.options_path = options_path
+    
+    def single_code_run(self, params: dict, run_dir: str, *args,**kwargs):
+        None
+        params['cell_edges'] 
+        params['pitch']
+        params['energy']
+        
+    
